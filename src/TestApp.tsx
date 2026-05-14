625fdedb73d07fb658faebd12c460203f3a30c06
Raquel Duarte <raquelduarteadmi@gmail.com>
2026-05-14 07:30:32 -0300

    chore: remove project files and configuration for EduTecPro

--- src/TestApp.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function TestApp() {
-  return (
-    <div style={{ padding: '20px', background: 'blue', color: 'white' }}>
-      <h1>React está funcionando!</h1>
-    </div>
-  );
-}
